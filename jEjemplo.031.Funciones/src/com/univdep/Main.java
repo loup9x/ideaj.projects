b98c2e897372604f1440cc5753b5e23c5bdfc2f2
loup9x <loup9x@gmail.com>
2022-04-22 08:11:09 -0500

    -

---
+++ jEjemplo.031.Funciones/src/com/univdep/Main.java
@@ -0,0 +1,19 @@
+package com.univdep;
+
+public class Main {
+    /*
+    Declaración de una función, en donde se realiza
+    la impresión de un simple mensaje de bienvenida.
+     */
+    public static void ImprimirMensaje() {
+        System.out.println("Hola Mundo!!!!");
+    }
+
+    public static void main(String[] args) {
+        // Se realiza la llamada o invocación a la función,
+        // y es en este momento, donde se desarrollan las
+        // sentencias, y concluyendo estas, vuelve a continuar
+        // el programa.
+	    ImprimirMensaje();
+    }
+}
