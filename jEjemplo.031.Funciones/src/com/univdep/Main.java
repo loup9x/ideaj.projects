4b0d0720ee83b2cbf56a06c3c2bbc1d5ca94848c
loup9x <loup9x@gmail.com>
2022-04-22 08:19:11 -0500

    -

--- jEjemplo.031.Funciones/src/com/univdep/Main.java
+++ jEjemplo.031.Funciones/src/com/univdep/Main.java
@@ -6,14 +6,28 @@ public class Main {
     la impresión de un simple mensaje de bienvenida.
      */
     public static void ImprimirMensaje() {
-        System.out.println("Hola Mundo!!!!");
+        System.out.println(":. Hola Mundo!!!!");
     }
 
+    /*
+    Declaración de una función con argumento, en donde se
+    realiza la impresión de un simple mensaje de
+    bienvenida.
+     */
+    public static void ImprimirTexto(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+
     public static void main(String[] args) {
-        // Se realiza la llamada o invocación a la función,
-        // y es en este momento, donde se desarrollan las
-        // sentencias, y concluyendo estas, vuelve a continuar
-        // el programa.
+        /*
+        Se realiza la llamada o invocación a la función,
+        y es en este momento, donde se desarrollan las
+        sentencias, y concluyendo estas, vuelve a continuar
+        el programa.
+        */
 	    ImprimirMensaje();
+        System.out.println();
+        ImprimirTexto(":: Hola mundo!!!\nFunción con argumentos");
     }
 }
