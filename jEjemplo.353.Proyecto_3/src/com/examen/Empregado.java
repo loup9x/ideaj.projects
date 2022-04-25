9c862c9ae6d9d9d1ac57a2abcf7db1fca41e4315
loup9x <loup9x@gmail.com>
2022-04-25 10:06:33 -0500

    -

---
+++ jEjemplo.353.Proyecto_3/src/com/examen/Empregado.java
@@ -0,0 +1,41 @@
+package com.examen;
+
+public class Empregado {
+    // Declaración de los atributos de la clase
+    private String Nome;
+    private String sobreNome;
+    private double salario;
+
+    // Declaración del constructor
+    public Empregado() {
+        this.Nome = "";
+        this.sobreNome = "";
+        this.salario = 0;
+    }
+
+    // Declaración de los métodos set/get de los atributos
+    // del objeto.
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getNome() {
+        return this.Nome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getSobreNome() {
+        return this.sobreNome;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+}
