439a1074473f771724c94ecbdc4c04e1057f6619
loup9x <loup9x@gmail.com>
2022-04-25 10:14:40 -0500

    -

--- jEjemplo.353.Proyecto_3/src/com/examen/Main.java
+++ jEjemplo.353.Proyecto_3/src/com/examen/Main.java
@@ -3,6 +3,19 @@ package com.examen;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Empregado A = new Empregado();
+        Empregado B = new Empregado();
+
+        A.setNome("Pedro");
+        A.setSobreNome("Godinez");
+        A.setSalario(4500);
+
+        B.setNome("Pablo");
+        B.setSobreNome("Gutierritos");
+        B.setSalario(4750);
+
+        System.out.println("Empregado \t Sobrenome \t\t Salario");
+        System.out.println(":." + A.getNome() + "\t\t " + A.getSobreNome() + "\t\t $ " + A.getSalario() +"\n"+
+                           ":." + B.getNome() + "\t\t " + B.getSobreNome() + "\t $ " + B.getSalario());
     }
 }
